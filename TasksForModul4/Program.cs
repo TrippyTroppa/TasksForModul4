6de29828113d96ce7be90a97ac75a063aa6691d1
Egor <egork2205@gmail.com>
2025-04-21 23:42:20 +0700

    различия кода с постусловием и предусловием

--- TasksForModul4/Program.cs
+++ TasksForModul4/Program.cs
@@ -49,7 +49,48 @@ namespace TasksForModul4
 
                 t++;
             }
-            while (t < 3);
+            while (t < -1);
+            int k = 0;
+
+            while (k < -1)
+            {
+                Console.WriteLine(k);
+
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+
+                k++;
+            }
+
         }
     }
 }
