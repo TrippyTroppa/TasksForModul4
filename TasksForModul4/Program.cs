6e34313b854d8945b30522f0886246766eb5dda5
Egor <egork2205@gmail.com>
2025-04-21 20:50:11 +0700

    Отработка Switch case

--- TasksForModul4/Program.cs
+++ TasksForModul4/Program.cs
@@ -13,26 +13,30 @@ namespace TasksForModul4
             Console.WriteLine("Введите свой любимый цвет на английском языке с маленькой буквы");
             var color = Console.ReadLine();
 
-            if (color == "red")
+            switch (color)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Your favourite color is red!");
-            }
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Your favourite color is green!");
-            }
+                    Console.WriteLine("Your color is red!");
+                    break;
 
-            else 
-            { 
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Your favourite color is unknown!");
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
             }
         }
     }
 }
+
