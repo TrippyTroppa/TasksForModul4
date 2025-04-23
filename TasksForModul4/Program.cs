b8512470db5875876daee125f277702a2ae63efb
Egor <egork2205@gmail.com>
2025-04-23 22:47:39 +0700

    Программа сортировки массива

--- TasksForModul4/Program.cs
+++ TasksForModul4/Program.cs
@@ -12,14 +12,25 @@ namespace TasksForModul4
         static void Main(string[] args)
         {
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-            int temp = 0;
+            int temp;
 
-            foreach (var item in arr)
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                        if (arr[i, j] > arr[i, k])
+                        {
+                            temp = arr[i, k];
+                            arr[i, k] = arr[i, j];
+                            arr[i, j] = temp;
+                        }
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
             {
-                if (item > 0)
-                temp++;
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine(temp);
         }
     }
 }
