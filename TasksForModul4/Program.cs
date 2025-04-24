9a7bc3fea7422ea5f71e45e056a20c6c86c835e0
Egor <egork2205@gmail.com>
2025-04-24 13:33:26 +0700

    Пример кортежа

--- TasksForModul4/Program.cs
+++ TasksForModul4/Program.cs
@@ -11,27 +11,13 @@ namespace TasksForModul4
     {
         static void Main(string[] args)
         {
-            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-            int temp;
+            var anketa = (name: "Jane", age: 27);
 
-            for (int i = 0; i <= arr.GetUpperBound(0); i++)
-                for (int j = 0; j <= arr.GetUpperBound(1); j++)
-                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
-                        if (arr[i, j] > arr[i, k])
-                        {
-                            temp = arr[i, k];
-                            arr[i, k] = arr[i, j];
-                            arr[i, j] = temp;
-                        }
-            for (int i = 0; i <= arr.GetUpperBound(0); i++)
-            {
-                for (int j = 0; j <= arr.GetUpperBound(1); j++)
-                {
-                    Console.Write(arr[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
         }
+    
     }
 }
 
